49296619cc8a1b639ad1330c631f3e55dc1a7337
leblancdev <evertonguittar@gmail.com>
2020-04-21 23:16:09 -0300

    mobile

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -6,10 +6,9 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
-        console.log(count);
 
         const incidents = await connection('incidents')
-            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
             .select(['incidents.*',
